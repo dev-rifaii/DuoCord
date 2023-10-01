3e4ba6229af4934cb1c63e6d7d59e51652be6943
Mohammad <moe.rifaii77@gmail.com>
2023-10-01 05:24:28 +0300

    Update stop_bot script

--- scripts/stop_bot.sh
+++ scripts/stop_bot.sh
@@ -1,1 +1,14 @@
-killall -9 java
+#!/bin/bash
+
+# Check if any Java processes are running
+java_pids=$(pgrep java)
+
+if [ -n "$java_pids" ]; then
+  # Stop all Java processes
+  for pid in $java_pids; do
+    kill -9 "$pid"
+    echo "Java process stopped (PID: $pid)"
+  done
+else
+  echo "No Java processes found"
+fi
