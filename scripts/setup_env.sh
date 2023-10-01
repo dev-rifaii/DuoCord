27a60a41ad7c7cf67284f24ba19b25ae349e1bc9
Mohammad <moe.rifaii77@gmail.com>
2023-10-01 21:33:03 +0300

    Export env variables only if they don't already exist

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -1,14 +1,30 @@
-DISCORD_BOT_KEY=$(aws ssm get-parameters --region us-east-1 --names /duocord/DISCORD_BOT_KEY --with-decryption --query Parameters[0].Value --output text)
-export DISCORD_BOT_KEY=$DISCORD_BOT_KEY
-echo "export DISCORD_BOT_KEY=$DISCORD_BOT_KEY" >> ~/.bashrc && source ~/.bashrc
+whoami
+if [ -z "$DISCORD_BOT_KEY" ]; then
+    DISCORD_BOT_KEY=$(aws ssm get-parameters --region us-east-1 --names /duocord/DISCORD_BOT_KEY --with-decryption --query Parameters[0].Value --output text)
+    echo "export DISCORD_BOT_KEY=$DISCORD_BOT_KEY" >> /root/.bashrc && source /root/.bashrc
+  else
+    echo "DISCORD_BOT_KEY is already set"
+fi
 
-REDIS_HOST=$(aws ssm get-parameters --region us-east-1 --names /duocord/REDIS_HOST --with-decryption --query Parameters[0].Value --output text)
-echo "$REDIS_HOST"
-echo "export REDIS_HOST=$REDIS_HOST" >> ~/.bashrc && source ~/.bashrc
+if [ -z "$REDIS_HOST" ]; then
+    REDIS_HOST=$(aws ssm get-parameters --region us-east-1 --names /duocord/REDIS_HOST --with-decryption --query Parameters[0].Value --output text)
+    echo "$REDIS_HOST"
+    echo "export REDIS_HOST=$REDIS_HOST" >> /root/.bashrc && source /root/.bashrc
+  else
+    echo "REDIS_HOST is already set"
+fi
 
-REDIS_PORT=$(aws ssm get-parameters --region us-east-1 --names /duocord/REDIS_PORT --with-decryption --query Parameters[0].Value --output text)
-echo "$REDIS_PORT"
-echo "export REDIS_PORT=$REDIS_PORT" >> ~/.bashrc && source ~/.bashrc
+if [ -z "$REDIS_PORT" ]; then
+    REDIS_PORT=$(aws ssm get-parameters --region us-east-1 --names /duocord/REDIS_PORT --with-decryption --query Parameters[0].Value --output text)
+    echo "$REDIS_PORT"
+    echo "export REDIS_PORT=$REDIS_PORT" >> /root/.bashrc && source /root/.bashrc
+  else
+    echo "REDIS_PORT is already set"
+fi
 
-REDIS_PASS=$(aws ssm get-parameters --region us-east-1 --names /duocord/REDIS_PASS --with-decryption --query Parameters[0].Value --output text)
-echo "export REDIS_PASS=$REDIS_PASS" >> ~/.bashrc && source ~/.bashrc
+if [ -z "$REDIS_PASS" ]; then
+    REDIS_PASS=$(aws ssm get-parameters --region us-east-1 --names /duocord/REDIS_PASS --with-decryption --query Parameters[0].Value --output text)
+    echo "export REDIS_PASS=$REDIS_PASS" >> /root/.bashrc && source /root/.bashrc
+  else
+    echo "REDIS_PASS is already set"
+fi
--- scripts/start_bot.sh
+++ scripts/start_bot.sh
@@ -1,3 +1,5 @@
+whoami
+source /root/.bashrc
 echo "$REDIS_PORT"
 
 nohup java -jar /tmp/DuoCord-0.1.jar dev.rifaii.Main
