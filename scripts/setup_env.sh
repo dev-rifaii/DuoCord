4ac11354ccbf7cb05736ea434f33d9184bee769a
Mohammad <moe.rifaii77@gmail.com>
2023-10-01 17:09:37 +0300

    Echo fetched parameters from AWS PS for debugging

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -2,9 +2,11 @@ DISCORD_BOT_KEY=$(aws ssm get-parameters --region us-east-1 --names /duocord/DIS
 export DISCORD_BOT_KEY=$DISCORD_BOT_KEY
 
 REDIS_HOST=$(aws ssm get-parameters --region us-east-1 --names /duocord/REDIS_HOST --with-decryption --query Parameters[0].Value --output text)
+echo "$REDIS_HOST"
 export REDIS_HOST=$REDIS_HOST
 
 REDIS_PORT=$(aws ssm get-parameters --region us-east-1 --names /duocord/REDIS_PORT --with-decryption --query Parameters[0].Value --output text)
+echo "$REDIS_PORT"
 export REDIS_PORT=$REDIS_PORT
 
 REDIS_PASS=$(aws ssm get-parameters --region us-east-1 --names /duocord/REDIS_PASS --with-decryption --query Parameters[0].Value --output text)
